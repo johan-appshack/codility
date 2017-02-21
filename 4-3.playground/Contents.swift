e4533777fcb3d10181e8691fbc3b130148d65eea
Johan Joensson <johan.joensson@appshack.se>
2017-02-21 12:24:29 +0100

    Permutation

---
+++ 4-3.playground/Contents.swift
@@ -0,0 +1,73 @@
+/*
+ A non-empty zero-indexed array A consisting of N integers is given.
+ 
+ A permutation is a sequence containing each element from 1 to N once, and only once.
+ 
+ For example, array A such that:
+ 
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ A[3] = 2
+ is a permutation, but array A such that:
+ 
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ is not a permutation, because value 2 is missing.
+ 
+ The goal is to check whether array A is a permutation.
+ 
+ Write a function:
+ 
+ public func solution(_ A : inout [Int]) -> Int
+ 
+ that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+ 
+ For example, given array A such that:
+ 
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ A[3] = 2
+ the function should return 1.
+ 
+ Given array A such that:
+ 
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ the function should return 0.
+ 
+ Assume that:
+ 
+ N is an integer within the range [1..100,000];
+ each element of array A is an integer within the range [1..1,000,000,000].
+ Complexity:
+ 
+ expected worst-case time complexity is O(N);
+ expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ Elements of input arrays can be modified.
+ */
+
+import UIKit
+
+public func solution(_ A : inout [Int]) -> Int {
+    var arr: [Int: Bool] = [:]
+    
+    for (_, val) in A.enumerated() {
+        arr[val] = true
+    }
+    
+    for index in 1...A.count {
+        guard let _ = arr[index] else {
+            return 0
+        }
+    }
+    
+    return 1
+}
+
+var arr = [9, 4, 1, 7, 6, 8, 3, 2, 5]
+
+solution(&arr)
